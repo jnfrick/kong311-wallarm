e9d599e3f6ac2d60b12fb6876313d7759429e9f9
EC2 Default User <ec2-user@ip-10-1-1-235.us-west-1.compute.internal>
2024-07-03 19:49:57 +0000

    update for Wallarm node 4.10.7

--- build_push.sh
+++ build_push.sh
@@ -1,5 +1,5 @@
 set -ex
 
-docker build --pull --progress=plain --tag jnfrick/kong311-wallarm:4.10.6 .
-docker push jnfrick/kong311-wallarm:4.10.6
+docker build --pull --progress=plain --tag jnfrick/kong311-wallarm:4.10.7 .
+docker push jnfrick/kong311-wallarm:4.10.7
 
